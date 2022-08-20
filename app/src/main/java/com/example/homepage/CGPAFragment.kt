ae87ca052356212e93c9feb5ea1fb478ddc00644
piru72 <parvezdirom2000@gmail.com>
2022-08-20 09:42:07 +0600

    Calculator visibility worked out

--- app/src/main/java/com/example/homepage/CGPAFragment.kt
+++ app/src/main/java/com/example/homepage/CGPAFragment.kt
@@ -1,16 +1,15 @@
 package com.example.homepage
 
-import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.Fragment
 
 
-class CGPAFragment : Fragment() {
+class CGPAFragment : ReplaceFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,32 +19,52 @@ class CGPAFragment : Fragment() {
 
         val v = inflater.inflate(R.layout.fragment_c_g_p_a, container, false)
 
-//        val spinner = v.findViewById<Spinner>(R.id.grade_list)
-//        val textInputBox = v.findViewById<EditText>(R.id.result_list)
-//        val btnCalculate = v.findViewById<Button>(R.id.btn_calculate)
-//        val textViewResult = v.findViewById<TextView>(R.id.result)
-//        val textViewGrade = v.findViewById<TextView>(R.id.grade)
-//
-//
-//
-//        btnCalculate.setOnClickListener{
-//            val text = spinner.selectedItem.toString()
-//            val text2 = textInputBox.text.toString()
-//            textViewResult.text = text
-//            textViewGrade.text = text2
-//            spinner.visibility = View.GONE;
-//            //spinner.visibility = View.VISIBLE;
-//
-//        }
+        val resultPan = v.findViewById<LinearLayout>(R.id.result_pan)
+        val btnSelect = v.findViewById<Button>(R.id.btn_select)
+        val semesterList = v.findViewById<Spinner>(R.id.semester_list)
+        val result = v.findViewById<TextView>(R.id.result)
+        val grade = v.findViewById<TextView>(R.id.grade)
+        val c10 = v.findViewById<LinearLayout>(R.id.c10)
+        val c9 = v.findViewById<LinearLayout>(R.id.c9)
+        val btnCalculate = v.findViewById<Button>(R.id.btn_calculate)
+
+        resultPan.visibility = View.GONE
+        btnCalculate.visibility = View.GONE
+        var currentSemester = ""
+
+        btnCalculate.setOnClickListener{
+            grade.text = currentSemester
 
 
+        }
+
+        btnSelect.setOnClickListener {
+            resultPan.isEnabled = true
+            btnCalculate.visibility = View.VISIBLE
+            currentSemester = semesterList.selectedItem.toString()
+            when (semesterList.selectedItem.toString()) {
+                "4th Year 1st Semester", "3rd Year 2nd Semester" -> {
+                    resultPan.visibility = View.VISIBLE
+                    c10.visibility = View.VISIBLE
+                    c9.visibility = View.VISIBLE
+
+                }
+                "4th Year 2nd Semester" -> {
+                    resultPan.visibility = View.VISIBLE
+                    c10.visibility = View.GONE
+                    c9.visibility = View.GONE
+                }
+                else -> {
+                    resultPan.visibility = View.VISIBLE
+                    c9.visibility = View.VISIBLE
+                    c10.visibility = View.GONE
+                }
+            }
+        }
 
 
         return v
     }
-
-
-
 
 
 }
