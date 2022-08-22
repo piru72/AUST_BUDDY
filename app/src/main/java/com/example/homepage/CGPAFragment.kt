7918f8362fa1c40897c457761a50028abce69088
piru72 <parvezdirom2000@gmail.com>
2022-08-22 08:40:59 +0600

    New design pattern implemented on CGPA page

--- app/src/main/java/com/example/homepage/CGPAFragment.kt
+++ app/src/main/java/com/example/homepage/CGPAFragment.kt
@@ -1,12 +1,10 @@
 package com.example.homepage
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.fragment.app.Fragment
 
 
 class CGPAFragment : ReplaceFragment() {
@@ -19,43 +17,57 @@ class CGPAFragment : ReplaceFragment() {
 
         val v = inflater.inflate(R.layout.fragment_c_g_p_a, container, false)
 
-        val resultPan = v.findViewById<LinearLayout>(R.id.result_pan)
+
+        //   All the layouts object
+
+        val layoutResultList = v.findViewById<LinearLayout>(R.id.layout_result_list)
+        val layoutCalculate = v.findViewById<LinearLayout>(R.id.layout_calculate)
+        val layoutResult = v.findViewById<LinearLayout>(R.id.layout_result)
+        val layoutInstruction = v.findViewById<LinearLayout>(R.id.layout_instruction)
+
+
+        // All the buttons object
         val btnSelect = v.findViewById<Button>(R.id.btn_select)
+
+
+
         val semesterList = v.findViewById<Spinner>(R.id.semester_list)
         val result = v.findViewById<TextView>(R.id.result)
-        val grade = v.findViewById<TextView>(R.id.grade)
         val c10 = v.findViewById<LinearLayout>(R.id.c10)
+
+
         val c9 = v.findViewById<LinearLayout>(R.id.c9)
         val btnCalculate = v.findViewById<Button>(R.id.btn_calculate)
 
-        resultPan.visibility = View.GONE
-        btnCalculate.visibility = View.GONE
-        var currentSemester = ""
 
-        btnCalculate.setOnClickListener{
-            grade.text = currentSemester
+        hideLayout(layoutResultList)
+        hideLayout(layoutResult)
+        hideLayout(layoutCalculate)
 
-
-        }
 
         btnSelect.setOnClickListener {
-            resultPan.isEnabled = true
+            layoutResultList.isEnabled = true
             btnCalculate.visibility = View.VISIBLE
-            currentSemester = semesterList.selectedItem.toString()
+            layoutResult.visibility = View.VISIBLE
+            layoutCalculate.visibility = View.VISIBLE
+            layoutInstruction.visibility = View.GONE
+
+
             when (semesterList.selectedItem.toString()) {
                 "4th Year 1st Semester", "3rd Year 2nd Semester" -> {
-                    resultPan.visibility = View.VISIBLE
+                    layoutResultList.visibility = View.VISIBLE
                     c10.visibility = View.VISIBLE
                     c9.visibility = View.VISIBLE
 
                 }
                 "4th Year 2nd Semester" -> {
-                    resultPan.visibility = View.VISIBLE
+                    layoutResultList.visibility = View.VISIBLE
                     c10.visibility = View.GONE
                     c9.visibility = View.GONE
                 }
                 else -> {
-                    resultPan.visibility = View.VISIBLE
+
+                    layoutResultList.visibility = View.VISIBLE
                     c9.visibility = View.VISIBLE
                     c10.visibility = View.GONE
                 }
@@ -64,6 +76,13 @@ class CGPAFragment : ReplaceFragment() {
 
 
         return v
+    }
+
+    private fun hideLayout(layout :LinearLayout) {
+        layout.visibility = View.GONE
+    }
+    private fun showLayout(layout :LinearLayout) {
+        layout.visibility = View.VISIBLE
     }
 
 
