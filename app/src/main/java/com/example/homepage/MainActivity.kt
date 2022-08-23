c1be621cf8c1e91e76f870f94d72b0d4bce4cbf9
piru72 <parvezdirom2000@gmail.com>
2022-08-23 07:55:42 +0600

    Initial pages added again

--- app/src/main/java/com/example/homepage/MainActivity.kt
+++ app/src/main/java/com/example/homepage/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.homepage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.homepage.databinding.ActivityMainHomePageBinding
 
@@ -10,42 +12,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_home_page)
+        setContentView(R.layout.welcome)
 
-        binding = ActivityMainHomePageBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        replaceFragment(HomeFragment())
-
-
-
-        binding.bottomNavigationView.setOnItemSelectedListener {
-
-            when (it.itemId) {
-
-                R.id.bot_nav_home -> replaceFragment(HomeFragment())
-                R.id.bot_nav_profile -> replaceFragment(ProfileFragment())
-                R.id.bot_nav_notice -> replaceFragment(NoticeFragment())
-                R.id.bot_nav_schedule -> replaceFragment(ScheduleFragment())
-                R.id.bot_nav_courses -> replaceFragment(CourseFragment())
-
-                else -> {
-
-                }
-
-            }
-            true
-
+        val nextButton = findViewById<Button>(R.id.get_started)
+        nextButton.setOnClickListener {
+            val intent = Intent(this, OnBoarding1::class.java)
+            startActivity(intent)
         }
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
 
-        fragmentTransaction.replace(R.id.frameLayout, fragment)
-        fragmentTransaction.commit()
 
-    }
+
+
+
+
 
 
 }
