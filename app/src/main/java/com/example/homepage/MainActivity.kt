b50b7215addb14292439e94f1fbf394d6082de0c
piru72 <parvezdirom2000@gmail.com>
2022-08-30 23:01:39 +0600

    App ready for debugging

--- app/src/main/java/com/example/homepage/MainActivity.kt
+++ app/src/main/java/com/example/homepage/MainActivity.kt
@@ -4,21 +4,79 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.example.homepage.databinding.ActivityMainHomePageBinding
 import com.example.homepage.onBoarding.OnBoarding1
+import com.example.homepage.profileTab.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+
+
+    //********************** For opening in the first onboarding page ************//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.welcome)
+//
+//        val nextButton = findViewById<Button>(R.id.get_started)
+//        nextButton.setOnClickListener {
+//            val intent = Intent(this, OnBoarding1::class.java)
+//            startActivity(intent)
+//        }
+//    }
+
+//********************** For opening in the third onboarding page ************//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_on_boarding3)
+//
+//        val nextButton = findViewById<Button>(R.id.onboarding_3_button)
+//        nextButton.setOnClickListener {
+//            val intent = Intent(this, MainHomePage::class.java)
+//            startActivity(intent)
+//        }
+//    }
+
+
+    //********************** For opening directly in the main Home Page onboarding page ************//
+
     private lateinit var binding: ActivityMainHomePageBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.welcome)
 
-        val nextButton = findViewById<Button>(R.id.get_started)
-        nextButton.setOnClickListener {
-            val intent = Intent(this, OnBoarding1::class.java)
-            startActivity(intent)
+        binding = ActivityMainHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when (it.itemId) {
+
+                R.id.bot_nav_home -> replaceFragment(HomeFragment())
+                R.id.bot_nav_profile -> replaceFragment(ProfileFragment())
+                R.id.bot_nav_notice -> replaceFragment(NoticeFragment())
+                R.id.bot_nav_schedule -> replaceFragment(ScheduleFragment())
+                R.id.bot_nav_courses -> replaceFragment(CourseFragment())
+
+                else -> {
+
+                }
+
+            }
+            true
+
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+
     }
 
 }
