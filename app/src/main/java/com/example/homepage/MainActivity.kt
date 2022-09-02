ef11daa3c80a96399e611827b9bf0ed3b055aa3d
piru72 <parvezdirom2000@gmail.com>
2022-09-03 00:35:25 +0600

    Schedule Fragment work started

--- app/src/main/java/com/example/homepage/MainActivity.kt
+++ app/src/main/java/com/example/homepage/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.homepage.databinding.ActivityMainHomePageBinding
 import com.example.homepage.onBoarding.OnBoarding1
+import com.example.homepage.profileTab.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,60 +40,60 @@ class MainActivity : AppCompatActivity() {
 
     //********************** For opening in the third onboarding to sign up page  page ************//
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding3)
-
-        val nextButton = findViewById<Button>(R.id.onboarding_3_button)
-        nextButton.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-          startActivity(intent)
-
-        }
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_on_boarding3)
+//
+//        val nextButton = findViewById<Button>(R.id.onboarding_3_button)
+//        nextButton.setOnClickListener {
+//            val intent = Intent(this, SignUpActivity::class.java)
+//          startActivity(intent)
+//
+//        }
+//    }
 
 
     //********************** For opening directly in the main Home Page onboarding page ************//
 
-//    private lateinit var binding: ActivityMainHomePageBinding
-//
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//        binding = ActivityMainHomePageBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//        replaceFragment(HomeFragment())
-//
-//
-//        binding.bottomNavigationView.setOnItemSelectedListener {
-//
-//            when (it.itemId) {
-//
-//                R.id.bot_nav_home -> replaceFragment(HomeFragment())
-//                R.id.bot_nav_profile -> replaceFragment(ProfileFragment())
-//                R.id.bot_nav_notice -> replaceFragment(NoticeFragment())
-//                R.id.bot_nav_schedule -> replaceFragment(ScheduleFragment())
-//                R.id.bot_nav_courses -> replaceFragment(ViewCourses())
-//
-//                else -> {
-//
-//                }
-//
-//            }
-//            true
-//
-//        }
-//    }
-//
-//    private fun replaceFragment(fragment: Fragment) {
-//        val fragmentManager = supportFragmentManager
-//        val fragmentTransaction = fragmentManager.beginTransaction()
-//
-//        fragmentTransaction.replace(R.id.frameLayout, fragment)
-//        fragmentTransaction.commit()
-//
-//    }
+    private lateinit var binding: ActivityMainHomePageBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when (it.itemId) {
+
+                R.id.bot_nav_home -> replaceFragment(HomeFragment())
+                R.id.bot_nav_profile -> replaceFragment(ProfileFragment())
+                R.id.bot_nav_notice -> replaceFragment(NoticeFragment())
+                R.id.bot_nav_schedule -> replaceFragment(ScheduleFragment())
+                R.id.bot_nav_courses -> replaceFragment(ViewCourses())
+
+                else -> {
+
+                }
+
+            }
+            true
+
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+
+    }
 
     ///small change done
 
--- app/src/main/java/com/example/homepage/ScheduleFragment.kt
+++ app/src/main/java/com/example/homepage/ScheduleFragment.kt
@@ -1,10 +1,18 @@
 package com.example.homepage
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.widget.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
 
 
 class ScheduleFragment : Fragment() {
@@ -16,145 +24,281 @@ class ScheduleFragment : Fragment() {
     ): View? {
 
         container?.removeAllViews()
-        val v = inflater.inflate(R.layout.fragment_schedule, container, false)
+        val v = inflater.inflate(R.layout.fragment_todo, container, false)
         //initOnOffButton(v)
         return v
     }
 
-//    private fun initOnOffButton(v:View) {
-//        val onOffButton = v.findViewById<Button>(R.id.onOffButton)
-//        onOffButton.setOnClickListener {
+
+
+//    private lateinit var binding: DataBinding
+//    private lateinit var database: DatabaseReference
+//    private lateinit var taskReference: DatabaseReference
+//    private lateinit var auth: FirebaseAuth
+//    private lateinit var recycler: RecyclerView
+//    private var adapter: TaskAdapter?= null
 //
-//            val model = it.tag as? AlarmDisplayModel ?: return@setOnClickListener
-//            val newModel = saveAlarmModel(model.hour, model.minute, model.onOff.not())
-//            renderView(newModel)
 //
-//            if (newModel.onOff) {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = DataBindingUtil.setContentView(this, com.google.firebase.database.R.layout.activity_todo)
+//        auth = Firebase.auth
+//        database = Firebase.database.reference
+//        val user = auth.currentUser!!.uid
+//        taskReference =  FirebaseDatabase.getInstance().getReference("user-tasks").child(user)
+//        recycler = findViewById(com.google.firebase.database.R.id.task_list)
+//        recycler.layoutManager = LinearLayoutManager(this)
 //
-//                val calendar = Calendar.getInstance().apply {
-//                    set(Calendar.HOUR_OF_DAY, newModel.hour)
-//                    set(Calendar.MINUTE, newModel.minute)
+//        //Floating action button start
+//        binding.floatingActionButton.setOnClickListener{view: View ->
 //
-//                    if (before(Calendar.getInstance())) {
-//                        add(Calendar.DATE, 1)
-//                    }
-//                }
+//            val rootLayout = layoutInflater.inflate(com.google.firebase.database.R.layout.custompopup, null)
 //
-//                val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-//                val intent = Intent(this, AlarmReceiver::class.java)
-//                val pendingIntent = PendingIntent.getBroadcast(this, ALARM_REQUEST_CODE,
-//                    intent, PendingIntent.FLAG_UPDATE_CURRENT)
 //
-//                alarmManager.setInexactRepeating(
-//                    AlarmManager.RTC_WAKEUP,
-//                    calendar.timeInMillis,
-//                    AlarmManager.INTERVAL_DAY,
-//                    pendingIntent
-//                )
+//            val taskName = rootLayout.findViewById<EditText>(com.google.firebase.database.R.id.TaskName)
+//            val taskDescription = rootLayout.findViewById<EditText>(com.google.firebase.database.R.id.TaskDescription)
+//            val closeButton = rootLayout.findViewById<Button>(com.google.firebase.database.R.id.CloseButton)
+//            val addButton = rootLayout.findViewById<Button>(com.google.firebase.database.R.id.AddButton)
 //
-//            } else {
-//                cancelAlarm()
+//            //Pop window object
+//            val popupWindow = PopupWindow(
+//                rootLayout,
+//                LinearLayout.LayoutParams.WRAP_CONTENT ,
+//                LinearLayout.LayoutParams.WRAP_CONTENT, true
+//            )
+//
+//            popupWindow.update();
+//            popupWindow.setElevation(10.5F)
+//            popupWindow.showAtLocation(
+//
+//                ToDoActivity, // Location to display popup window
+//                Gravity.CENTER, // Exact position of layout to display popup
+//                0, // X offset
+//                0 // Y offset
+//            )
+//
+//            closeButton.setOnClickListener{
+//                popupWindow.dismiss()
 //            }
 //
+//            addButton.setOnClickListener{
+//
+//                var name = taskName.text.toString()
+//                var description = taskDescription.text.toString()
+//                val userId = user
+//                if(userId!= null){
+//                    writeNewTask(userId,name, description)}
+//                popupWindow.dismiss()
+//            }
+//
+//        }// end of floating action
+//
+//    }// end of on create-----------------------------------------------------------
+//
+//
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        val inflater: MenuInflater = menuInflater
+//        inflater.inflate(com.google.firebase.database.R.menu.menu_options, menu)
+//        return true
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+//        com.google.firebase.database.R.id.sign_out -> {
+//            // User chose the "Settings" item, show the app settings UI...
+//            val intent = Intent(this, LoginActivity::class.java)
+//            startActivity(intent)
+//            FirebaseAuth.getInstance().signOut()
+//            true
+//        }
+//
+//        else -> {
+//            // If we got here, the user's action was not recognized.
+//            // Invoke the superclass to handle it.
+//            super.onOptionsItemSelected(item)
 //        }
 //    }
 //
-//    private fun initChangeAlarmTimeButton() {
-//        val changeAlarmButton = findViewById<Button>(R.id.changeAlarmTimeButton)
-//        changeAlarmButton.setOnClickListener {
-//
-//            val calendar = Calendar.getInstance()
-//            TimePickerDialog(this, { picker, hour, minute ->
-//
-//                val model = saveAlarmModel(hour, minute, false)
-//                renderView(model,v)
-//                cancelAlarm()
-//
-//            }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false).show()
-//
+//    private fun writeNewTask(userId: String, taskname: String, taskdescription: String) {
+//        // Create new post at /user-posts/$userid/$postid and at
+//        // /posts/$postid simultaneously
+//        val key = database.child("posts").push().key
+//        if (key == null) {
+//            Log.w("TodoActivity", "Couldn't get push key for posts")
+//            return
 //        }
 //
-//    }
-//
-//    private fun saveAlarmModel(
-//        hour: Int,
-//        minute: Int,
-//        onOff: Boolean
-//    ): AlarmDisplayModel {
-//        val model = AlarmDisplayModel(
-//            hour = hour,
-//            minute = minute,
-//            onOff = onOff
+//        val newtask = Tasks(userId, taskname, taskdescription)
+//        val taskValues = newtask.toMap()
+//        val childUpdates = hashMapOf<String, Any>(
+//            //*   "/tasks/$key" to taskValues,
+//            "/user-tasks/$userId/$key" to taskValues
 //        )
 //
-//        val sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
-//
-//        with(sharedPreferences.edit()) {
-//            putString(ALARM_KEY, model.makeDataForDB())
-//            putBoolean(ONOFF_KEY, model.onOff)
-//            commit()
-//        }
-//
-//        return model
-//    }
-////
-////    private fun fetchDataFromSharedPreferences(): AlarmDisplayModel {
-////        val sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
-////
-////        val timeDBValue = sharedPreferences.getString(ALARM_KEY, "9:30") ?: "9:30"
-////        val onOffDBValue = sharedPreferences.getBoolean(ONOFF_KEY, false)
-////        val alarmData = timeDBValue.split(":")
-////
-////        val alarmModel = AlarmDisplayModel(
-////            hour = alarmData[0].toInt(),
-////            minute = alarmData[1].toInt(),
-////            onOff = onOffDBValue
-////        )
-////
-////
-////
-////        val pendingIntent = PendingIntent.getBroadcast(this, ALARM_REQUEST_CODE, Intent(this, AlarmReceiver::class.java), PendingIntent.FLAG_NO_CREATE)
-////
-////        if ((pendingIntent == null) and alarmModel.onOff) {
-////
-////            alarmModel.onOff = false
-////
-////        } else if ((pendingIntent != null) and alarmModel.onOff.not()){
-////
-////            pendingIntent.cancel()
-////        }
-////
-////        return alarmModel
-////
-////    }
-////
-//    private fun renderView(model: AlarmDisplayModel,v:View) {
-//        findViewById<TextView>(R.id.ampmTextView).apply {
-//            text = model.ampmText
-//        }
-//
-//        findViewById<TextView>(R.id.timeTextView).apply {
-//            text = model.timeText
-//        }
-//
-//        findViewById<Button>(R.id.onOffButton).apply {
-//            text = model.onOffText
-//            tag = model
-//        }
+//        database.updateChildren(childUpdates)
 //
 //    }
+//
+//    public override fun onStart() {
+//        super.onStart()
+//        adapter = TaskAdapter(this, taskReference)
+//        recycler.adapter = adapter
+//    }
+//
+//
+//
+//
+//    //-----------------------------Adapter Class-----------------------------------------------
+//    private class TaskAdapter(private val context: Context, private val databaseReference: DatabaseReference): RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+//
+//        private val childEventListener: ChildEventListener?
+//        private val taskIds = ArrayList<String>()
+//        private val tasks = ArrayList<Tasks>()
+//
+//        init {
+//
+//            // Create child event listener
+//            // [START child_event_listener_recycler]
+//            val childEventListener = object : ChildEventListener {
+//                override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
+//                    Log.d("GetData", "onChildAdded:" + dataSnapshot.key!!)
+//
+//                    // A new comment has been added, add it to the displayed list
+//                    val taskreceived = dataSnapshot.getValue<Tasks>()
+//
+//                    // [START_EXCLUDE]
+//                    // Update RecyclerView
+//                    taskIds.add(dataSnapshot.key!!)
+//                    tasks.add(taskreceived!!)
+//                    notifyItemInserted(tasks.size - 1)
+//                    // [END_EXCLUDE]
+//                }
+//
+//                override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String?) {
+//                    Log.d("GetData", "onChildChanged: ${dataSnapshot.key}")
+//
+////                    // A comment has changed, use the key to determine if we are displaying this
+////                    // comment and if so displayed the changed comment.
+////                    val newComment = dataSnapshot.getValue<Tasks>()
+////                    val commentKey = dataSnapshot.key
 ////
-////    private fun cancelAlarm() {
-////        val pendingIntent = PendingIntent.getBroadcast(this, ALARM_REQUEST_CODE, Intent(this, AlarmReceiver::class.java), PendingIntent.FLAG_NO_CREATE)
-////        pendingIntent?.cancel()
-////    }
+////                    // [START_EXCLUDE]
+////                    val commentIndex = taskIds.indexOf(commentKey)
+////                    if (commentIndex > -1 && newComment != null) {
+////                        // Replace with the new data
+////                        tasks[commentIndex] = newComment
+////
+////                        // Update the RecyclerView
+////                        notifyItemChanged(commentIndex)
+////                    } else {
+////                        Log.w("GetData", "onChildChanged:unknown_child: $commentKey")
+////                    }
+//                    // [END_EXCLUDE]
+//                }
 //
-//    companion object {
-//        private const val SHARED_PREFERENCES_NAME = "time"
-//        private const val ALARM_KEY = "alarm"
-//        private const val ONOFF_KEY = "onOff"
-//        private const val ALARM_REQUEST_CODE = 1000
+//                override fun onChildRemoved(dataSnapshot: DataSnapshot) {
+//                    Log.d("GetData", "onChildRemoved:" + dataSnapshot.key!!)
 //
+//                    // A comment has changed, use the key to determine if we are displaying this
+//                    // comment and if so remove it.
+//                    val commentKey = dataSnapshot.key
+//
+//                    // [START_EXCLUDE]
+//                    val commentIndex = taskIds.indexOf(commentKey)
+//                    if (commentIndex > -1) {
+//                        // Remove data from the list
+//                        taskIds.removeAt(commentIndex)
+//                        tasks.removeAt(commentIndex)
+//
+//                        // Update the RecyclerView
+//                        notifyItemRemoved(commentIndex)
+//                    } else {
+//                        Log.w("GetData", "onChildRemoved:unknown_child:" + commentKey!!)
+//                    }
+//                    // [END_EXCLUDE]
+//                }
+//
+//                override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String?) {
+////                    Log.d("GetData", "onChildMoved:" + dataSnapshot.key!!)
+////
+////                    // A comment has changed position, use the key to determine if we are
+////                    // displaying this comment and if so move it.
+////                    val movedComment = dataSnapshot.getValue<Tasks>()
+////                    val commentKey = dataSnapshot.key
+//
+//                    // ...
+//                }
+//
+//                override fun onCancelled(databaseError: DatabaseError) {
+//                    Log.w("GetData", "postComments:onCancelled", databaseError.toException())
+//                    Toast.makeText(context, "Failed to load comments.",
+//                        Toast.LENGTH_SHORT).show()
+//                }
+//
+//
+//            }
+//            databaseReference.addChildEventListener(childEventListener)
+//            // [END child_event_listener_recycler]
+//            // Store reference to listener so it can be removed on app stop
+//            this.childEventListener = childEventListener
+//
+//        } //end of Init()
+//
+//
+//        class TaskViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+//            val auth = Firebase.auth
+//            val user = auth.currentUser!!.uid
+//            val taskReference =  FirebaseDatabase.getInstance().getReference("user-tasks").child(user)
+//
+//            fun bind(task: Tasks, taskId: String) {
+//                itemView.taskName.text = task.taskname
+//                itemView.taskDescription.text = task.taskdescription
+//                val item = taskId
+//                itemView.deleteButton.setOnClickListener{
+//                    val value= taskReference.child(taskId)
+//                    value.removeValue()
+//                    Log.i("TodoActivity","VALUE Deleted")
+//                }
+//            }
+//        }
+//
+//        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+//            val inflater = LayoutInflater.from(context)
+//            val view = inflater.inflate(com.google.firebase.database.R.layout.list_item_view, parent, false)
+//            return TaskViewHolder(view)
+//
+//        }
+//
+//        override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+//            holder.bind(tasks[position], taskIds[position])
+//            holder.itemView.checkBoxItem.setOnClickListener{
+//            }
+//        }
+//
+//        override fun getItemCount(): Int = tasks.size
+//
+//        fun cleanupListener() {
+//            childEventListener?.let {
+//                databaseReference.removeEventListener(it)
+//            }
+//        }
+//    } // end of adapter class
+//
+//} // end of Todo Activity
+//
+//
+//// Task data class
+//data class Tasks(
+//
+//    var uid: String? ="",
+//    var taskname: String? ="",
+//    var taskdescription: String? = ""
+//){
+//    fun toMap(): Map<String, Any?>{
+//        return mapOf(
+//            "uid" to uid,
+//            "taskname" to taskname,
+//            "taskdescription" to taskdescription
+//        )
 //    }
 
 
