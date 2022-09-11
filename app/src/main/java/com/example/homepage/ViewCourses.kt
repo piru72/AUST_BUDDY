6b7fa7a69412692a0edb50e75af9840629100f99
piru72 <parvezdirom2000@gmail.com>
2022-09-11 11:10:19 +0600

    view course fragment cleared

--- app/src/main/java/com/example/homepage/ViewCourses.kt
+++ app/src/main/java/com/example/homepage/ViewCourses.kt
@@ -30,17 +30,6 @@ private lateinit var userRecyclerView: RecyclerView
 lateinit var adapter: MyAdapter
 
 class ViewCourses : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -50,30 +39,9 @@ class ViewCourses : Fragment() {
         val v = inflater.inflate(R.layout.fragment_view_courses, container, false)
 
         return v
-
-
-
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ViewCourses.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ViewCourses().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
