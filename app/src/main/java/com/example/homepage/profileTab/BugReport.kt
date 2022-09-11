fd35fee97b73c3eb7c233bff4128acb8ba783996
piru72 <parvezdirom2000@gmail.com>
2022-09-11 10:43:15 +0600

    Bug report button activated

--- app/src/main/java/com/example/homepage/profileTab/BugReport.kt
+++ app/src/main/java/com/example/homepage/profileTab/BugReport.kt
@@ -1,10 +1,14 @@
 package com.example.homepage.profileTab
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.example.homepage.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -22,9 +26,24 @@ class BugReport : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         container?.removeAllViews()
         val v = inflater.inflate(R.layout.fragment_bug_report, container, false)
+
+        val sendBTn = v.findViewById<Button>(R.id.bugReportBtn)
+
+        sendBTn.setOnClickListener {
+            val email = "unibuddy890@gmail.com"
+            val message = v.findViewById<TextView>(R.id.message)
+            val addresses = email.split(",".toRegex()).toTypedArray()
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, addresses)
+                putExtra(Intent.EXTRA_SUBJECT, "FIX THE BUG OF UNIBUDDY")
+                putExtra(Intent.EXTRA_TEXT,  message.text.toString())
+            }
+            startActivity(intent)
+            message.text =" "
+        }
         return v
     }
 }
