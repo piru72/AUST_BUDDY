85527a144d8b7fbed5cacbd0c03ce876ff2cbce9
piru72 <parvezdirom2000@gmail.com>
2022-10-16 22:27:05 +0600

    fix(webView): checked when device is offline

--- app/src/main/java/com/example/homepage/superClass/ReplaceFragment.kt
+++ app/src/main/java/com/example/homepage/superClass/ReplaceFragment.kt
@@ -1,11 +1,14 @@
 package com.example.homepage.superClass
 
 import android.annotation.SuppressLint
+import android.content.Context.CONNECTIVITY_SERVICE
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.net.Uri
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.homepage.R
 
@@ -22,7 +25,7 @@ open class ReplaceFragment : Fragment() {
     }
 
 
-    fun replaceFragment(fragment: Fragment, xml_file_name: Int,message :String) {
+    fun replaceFragment(fragment: Fragment, xml_file_name: Int, message: String) {
         val fragmentManager = requireActivity().supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(xml_file_name, fragment)
@@ -32,14 +35,34 @@ open class ReplaceFragment : Fragment() {
 
     @SuppressLint("SetJavaScriptEnabled")
     fun loadWebSite(webSite: String, v: View) {
+
+
         val webView = v.findViewById<WebView>(R.id.mWebView)
         webView.webViewClient = WebViewClient()
-        webView.canGoBack()
-        webView.apply {
-            loadUrl(webSite)
-            settings.javaScriptEnabled = true
-            settings.safeBrowsingEnabled = true
+
+        val connectivityManager =
+            context?.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (!(connectivityManager.activeNetworkInfo != null && connectivityManager.activeNetworkInfo!!.isAvailable && connectivityManager.activeNetworkInfo!!.isConnected)) {
+            Toast.makeText(context, "No Internet Connection!", Toast.LENGTH_SHORT).show()
         }
+        else
+        {
+            webView.apply {
+                loadUrl(webSite)
+                settings.javaScriptEnabled = true
+                settings.setSupportZoom(true)
+                settings.safeBrowsingEnabled = true
+                settings.allowFileAccess = true
+                settings.domStorageEnabled = true
+                settings.javaScriptCanOpenWindowsAutomatically = true
+                settings.supportMultipleWindows()
+            }
+
+        }
+
+
+
 
     }
 
