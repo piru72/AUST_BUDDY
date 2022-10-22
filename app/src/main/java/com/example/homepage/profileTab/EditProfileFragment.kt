8be05dfcf9575c975a0fd019d8a43004a398a5dd
piru72 <parvezdirom2000@gmail.com>
2022-10-23 00:56:37 +0600

    try(location): tried to find out current location

--- app/src/main/java/com/example/homepage/profileTab/EditProfileFragment.kt
+++ app/src/main/java/com/example/homepage/profileTab/EditProfileFragment.kt
@@ -1,18 +1,28 @@
 package com.example.homepage.profileTab
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import com.example.homepage.R
 import com.example.homepage.superClass.ReplaceFragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseAuth
 
 
 class EditProfileFragment : ReplaceFragment() {
 
     private val user = FirebaseAuth.getInstance().currentUser
+    private var latitude =0.0
+    private var longitude =0.0
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,7 +30,9 @@ class EditProfileFragment : ReplaceFragment() {
     ): View? {
         // Inflate the layout for this fragment
         container?.removeAllViews()
+
         val v = inflater.inflate(R.layout.fragment_edit_profile, container, false)
+        fusedLocationClient = context?.let { LocationServices.getFusedLocationProviderClient(it) }!!
         val email = user?.email.toString()
         setInformation(email)
 
@@ -35,8 +47,42 @@ class EditProfileFragment : ReplaceFragment() {
         userDept.text = getDepartment()
         userSession.text = getSession()
         userName.text = getUserName()
+        obtainedLocalization()
+
+        makeToast(latitude.toString())
+        makeToast(longitude.toString())
 
         return v
+    }
+
+
+    private fun obtainedLocalization(){
+        if (context?.let {
+                ActivityCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            } != PackageManager.PERMISSION_GRANTED && context?.let {
+                ActivityCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            } != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                latitude = location?.latitude!!
+                longitude = location.longitude
+            }
     }
 
 
