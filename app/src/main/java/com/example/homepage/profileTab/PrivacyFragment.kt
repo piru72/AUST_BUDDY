1c80d749ea5799a3776ff4a7225e0008d5fbfd4b
piru72 <parvezdirom2000@gmail.com>
2022-10-23 01:21:56 +0600

    add(privacy policy): privacy policy page added

--- app/src/main/java/com/example/homepage/profileTab/PrivacyFragment.kt
+++ app/src/main/java/com/example/homepage/profileTab/PrivacyFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.example.homepage.R
 
 
 class PrivacyFragment : Fragment() {
 
-
+    private lateinit var webViewPage: WebView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,6 +20,25 @@ class PrivacyFragment : Fragment() {
         // Inflate the layout for this fragment
         container?.removeAllViews()
         return inflater.inflate(R.layout.fragment_privacy, container, false)
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        webViewPage  = view.findViewById(R.id.webView)
+
+
+        webViewPage.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+        webViewPage.loadUrl("file:///android_asset/privacyPolicy.html")
     }
 
 
