46ff1d84c91848a7665418c5d489b7a6d09cdee5
piru72 <parvezdirom2000@gmail.com>
2022-11-06 06:44:04 +0600

    fix(home page): binding implemented

--- app/src/main/java/com/example/homepage/HomeFragment.kt
+++ app/src/main/java/com/example/homepage/HomeFragment.kt
@@ -4,68 +4,58 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import com.example.homepage.calculator.CGPAFragment
-import com.example.homepage.homeTab.*
+import com.example.homepage.databinding.FragmentHomeBinding
+import com.example.homepage.homeTab.BusFragment
+import com.example.homepage.homeTab.GradingsFragment
+import com.example.homepage.homeTab.RequestFragment
 import com.example.homepage.superClass.ReplaceFragment
 
 
 class HomeFragment : ReplaceFragment() {
-
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         container?.removeAllViews()
-        val v = inflater.inflate(R.layout.fragment_home, container, false)
-        val btnIums = v.findViewById<Button>(R.id.btn_iums)
-        val btnAustOj = v.findViewById<Button>(R.id.btn_aust_oj)
-        val btnTeachers = v.findViewById<Button>(R.id.btn_teachers)
-        val btnSyllabus = v.findViewById<Button>(R.id.btn_syllabus)
-        val btnCalender = v.findViewById<Button>(R.id.btn_calender)
-        val btnBus = v.findViewById<Button>(R.id.btn_buses)
-        val btnCgpa = v.findViewById<Button>(R.id.btn_cgpa)
-        val btnGradingSystem = v.findViewById<Button>(R.id.btn_gradings)
-        val btnRequest = v.findViewById<Button>(R.id.btn_request)
-        val btnNotice = v.findViewById<Button>(R.id.noticeButton)
-
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val currentState = R.id.fragment_home
 
-        btnIums.setOnClickListener {
-            replaceFragment(WebView("https://iums.aust.edu/ums-web/login/"),currentState)
+        binding.btnIums.setOnClickListener {
+            replaceFragment(WebView(getString(R.string.universityStudentPortalLink)),currentState)
         }
-        btnAustOj.setOnClickListener {
-
-            replaceFragment(WebView("https://austoj.com/"),currentState)
+        binding.btnAustOj.setOnClickListener {
+            replaceFragment(WebView(getString(R.string.universityOjLink)),currentState)
         }
-        btnTeachers.setOnClickListener {
+        binding.btnTeachers.setOnClickListener {
             replaceFragment(GradingsFragment(), currentState)
         }
-        btnSyllabus.setOnClickListener {
-            replaceFragment(WebView("https://www.aust.edu/cse/syllabus#spring2020"),currentState)
+        binding.btnSyllabus.setOnClickListener {
+            replaceFragment(WebView(getString(R.string.universitySyllabusLink)),currentState)
         }
-        btnCalender.setOnClickListener {
-            replaceFragment(WebView("https://www.aust.edu/academics/academic_calendar"),currentState)
+        binding.btnCalender.setOnClickListener {
+            replaceFragment(WebView(getString(R.string.universityAcademicCalenderLink)),currentState)
         }
-        btnBus.setOnClickListener {
+        binding.btnBuses.setOnClickListener {
             replaceFragment(BusFragment(), currentState)
         }
-        btnCgpa.setOnClickListener {
+        binding.btnCgpa.setOnClickListener {
             replaceFragment(CGPAFragment(), currentState)
         }
-        btnGradingSystem.setOnClickListener {
-            replaceFragment(WebView("https://www.aust.edu/academics/examincation_and_grading_system"),currentState)
+        binding.btnGradings.setOnClickListener {
+            replaceFragment(WebView(getString(R.string.universityGradingSystemLink)),currentState)
         }
-        btnRequest.setOnClickListener {
+        binding.btnRequest.setOnClickListener {
             replaceFragment(RequestFragment(), currentState)
         }
-
-        btnNotice.setOnClickListener{
-            replaceFragment(WebView("https://www.aust.edu/notice"),currentState)
+        binding.noticeButton.setOnClickListener{
+            replaceFragment(WebView(getString(R.string.universityNoticeLink)),currentState)
         }
 
-        return v
+        return binding.root
     }
 
 
