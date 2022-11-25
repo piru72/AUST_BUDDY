660512239349948c30188153f59fed173e26234b
piru72 <parvezdirom2000@gmail.com>
2022-11-25 20:43:50 +0600

    added (schedules): added schedules life cycle

---
+++ app/src/main/java/com/example/homepage/scheduleTab/scheduleModel/ScheduleData.kt
@@ -0,0 +1,19 @@
+package com.example.homepage.scheduleTab.scheduleModel
+
+data class ScheduleData(
+
+    var uid: String? = "",
+    var taskname: String? = "",
+    var taskdescription: String? = "",
+    var taskdate: String? = ""
+) {
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "uid" to uid,
+            "taskname" to taskname,
+            "taskdescription" to taskdescription,
+            "taskdate" to taskdate
+        )
+    }
+
+}
