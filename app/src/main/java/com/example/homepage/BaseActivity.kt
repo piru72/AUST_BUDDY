5854d6ad29e4701ab61ff84552f857e90414043a
piru72 <parvezdirom2000@gmail.com>
2022-12-10 01:17:40 +0600

    fix (colors) : dark theme implemented

--- app/src/main/java/com/example/homepage/BaseActivity.kt
+++ app/src/main/java/com/example/homepage/BaseActivity.kt
@@ -3,8 +3,6 @@ package com.example.homepage
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.homepage.databinding.ActivityBaseBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -24,13 +22,13 @@ class BaseActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_base)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_departmentSemesterChooser, R.id.navigation_userGroups,R.id.navigation_profile
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        supportActionBar?.hide()
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_home, R.id.navigation_departmentSemesterChooser, R.id.navigation_userGroups,R.id.navigation_profile
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        supportActionBar?.hide()
         navView.setupWithNavController(navController)
     }
 }
