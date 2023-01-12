8c5efe03aeec04c7b32d497d21804d4788f059ff
piru72 <parvezdirom2000@gmail.com>
2023-01-13 05:21:02 +0600

    fix (plaza) : floating button

--- app/src/main/java/com/example/homepage/plaza/PlazaFragment.kt
+++ app/src/main/java/com/example/homepage/plaza/PlazaFragment.kt
@@ -1,17 +1,22 @@
 package com.example.homepage.plaza
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.*
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.homepage.R
 import com.example.homepage.databinding.FragmentPlazaBinding
 import com.example.homepage.plaza.Adapter.PlazaAdapter
 import com.example.homepage.plaza.Model.PlazaViewModel
+import com.example.homepage.storeTab.Model.Materials
+import com.example.homepage.superClass.ReplaceFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
@@ -19,7 +24,7 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 
-class PlazaFragment : Fragment() {
+class PlazaFragment : ReplaceFragment() {
 
 
     private var _binding: FragmentPlazaBinding? = null
@@ -30,6 +35,7 @@ class PlazaFragment : Fragment() {
     private var adapter: PlazaAdapter? = null
     private lateinit var database: DatabaseReference
     private lateinit var auth: FirebaseAuth
+    private val userV = FirebaseAuth.getInstance().currentUser
 
 
     override fun onCreateView(
@@ -44,7 +50,120 @@ class PlazaFragment : Fragment() {
             val action = PlazaFragmentDirections.actionPlazaFragmentToPlazaDashBoardFragment()
             findNavController().navigate(action)
         }
+        val user = auth.currentUser!!.uid
+
+        binding.floatingPostItemButton.setOnClickListener {
+            val rootLayout = layoutInflater.inflate(R.layout.popup_add_item_for_sell, null)
+
+            val productName = rootLayout.findViewById<EditText>(R.id.productNamePop)
+            val productAuthor = rootLayout.findViewById<EditText>(R.id.productAuthorPop)
+            val productCategory = rootLayout.findViewById<Spinner>(R.id.bookCategoryList)
+            val productPrice = rootLayout.findViewById<EditText>(R.id.productPricePop)
+            val productDetails = rootLayout.findViewById<EditText>(R.id.productDetailsPop)
+            val sellersContactNo = rootLayout.findViewById<EditText>(R.id.sellerContactNoPop)
+            val addButton = rootLayout.findViewById<Button>(R.id.AddButton)
+            val closeButton = rootLayout.findViewById<Button>(R.id.CloseButton)
+
+            val popupWindow = PopupWindow(
+                rootLayout,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT, true
+            )
+
+            popupWindow.update()
+            popupWindow.elevation = 20.5F
+            popupWindow.showAtLocation(
+
+                binding.postedItemsListLayout,
+                Gravity.CENTER,
+                0,
+                -500
+            )
+
+            closeButton.setOnClickListener {
+                popupWindow.dismiss()
+            }
+
+
+            addButton.setOnClickListener {
+
+                val productNameWrite = productName.text.toString()
+                val productAuthorWrite = productAuthor.text.toString()
+                val productCategoryWrite = productCategory.selectedItem.toString()
+                val productPriceWrite = productPrice.text.toString()
+                val productDetailsWrite = productDetails.text.toString()
+                val sellersContactNoWrite = sellersContactNo.text.toString()
+                val email = userV?.email.toString()
+                setInformation(email)
+                val sellersDetailsWrite =
+                    sellersContactNoWrite + " " + getUserName() + " " + getUserEmail() + " " + getUserId() + " " + getSession() + " " + getDepartment()
+
+
+                if (productNameWrite == "")
+                    makeToast("Please fill up all Product Name")
+                else if (productCategoryWrite == "Category")
+                    makeToast("Please fill up Product Category")
+                else if (productPriceWrite == "")
+                    makeToast("Please fill up Product Price")
+                else if (!validNumber(sellersContactNoWrite))
+                    makeToast("Provide 11 digit valid phone number")
+                else {
+
+                    addNewMaterial(
+                        user,
+                        productNameWrite,
+                        productAuthorWrite,
+                        productCategoryWrite,
+                        productPriceWrite,
+                        sellersContactNoWrite,
+                        sellersDetailsWrite,
+                        productDetailsWrite
+                    )
+                    popupWindow.dismiss()
+                }
+
+
+            }
+
+
+        }
         return binding.root
+    }
+
+    private fun addNewMaterial(
+        userId: String,
+        productName: String,
+        productAuthor: String,
+        productCategory: String,
+        productPrice: String,
+        sellersContactNo: String,
+        sellersDetails: String,
+        productDetailsWrite: String
+    ) {
+        val key = database.child("posts").push().key
+
+        if (key == null) {
+            Log.w("TodoActivity", "Couldn't get push key for posts")
+            return
+        }
+        val newMaterial = Materials(
+            userId,
+            productName,
+            productAuthor,
+            productCategory,
+            productPrice,
+            sellersContactNo,
+            sellersDetails,
+            key,
+            productDetailsWrite
+        )
+        val taskValues = newMaterial.toMap()
+        val childUpdates = hashMapOf<String, Any>(
+            "/user-posted-items/$userId/$key" to taskValues,
+            "/public-posts/$key" to taskValues
+        )
+        database.updateChildren(childUpdates)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
