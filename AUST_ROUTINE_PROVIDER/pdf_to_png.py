e2acc1171a8615d4d49618f777f77afc1e04a7da
piru72 <parvezdirom2000@gmail.com>
2023-02-03 07:58:42 +0600

    All semester routine updated

---
+++ AUST_ROUTINE_PROVIDER/pdf_to_png.py
@@ -0,0 +1,20 @@
+# import modules
+import os
+from pdf2image import convert_from_path
+
+# Define the directory that contains the PDF files
+pdf_dir = 'OutputPDF'
+
+# Get a list of all PDF files in the directory
+pdf_files = [f for f in os.listdir(pdf_dir) if f.endswith('.pdf')]
+
+# Loop through each PDF file
+for pdf_file in pdf_files:
+    full_path = os.path.join(pdf_dir, pdf_file)
+    images = convert_from_path(full_path)
+    
+    # Save each page of the PDF as an image
+    image_name = pdf_file[:-4]
+
+    for i in range(len(images)):
+        images[i].save('OutputImages/' + image_name + '.png', 'PNG')
--- AUST_ROUTINE_PROVIDER/pdf_to_png_converter.rb
+++
@@ -1,9 +0,0 @@
-require "mini_magick"
-
-# Open the PDF
-pdf = MiniMagick::Image.open("Year_1_Semester_1_SectionA_CSE.pdf")
-
-# Convert the first page of the PDF to an image
-image = pdf.clone
-image.format "png"
-image.write "page1.png"
