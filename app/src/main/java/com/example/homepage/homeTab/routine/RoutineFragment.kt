5468be509444fd64f29e22c202cca69ac609fdc7
piru72 <parvezdirom2000@gmail.com>
2023-02-16 09:55:33 +0600

    fix (routine) : dynamic routine

--- app/src/main/java/com/example/homepage/homeTab/routine/RoutineFragment.kt
+++ app/src/main/java/com/example/homepage/homeTab/routine/RoutineFragment.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.example.homepage.dataClass.UserAllData
 import com.example.homepage.databinding.FragmentRoutineBinding
 import com.example.homepage.homeTab.routine.model.Routine
 import com.example.homepage.superClass.ReplaceFragment
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -25,23 +27,55 @@ class RoutineFragment : ReplaceFragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentRoutineBinding.inflate(inflater, container, false)
+        val context = activity
 
-        val path = "/routines/cse/year3/semester1/sectionC"
-        var routineLink = "Not Found"
-        val databaseReference = FirebaseDatabase.getInstance().getReference(path)
-        val postListener = object : ValueEventListener {
+        val user = FirebaseAuth.getInstance().currentUser
+        val email = user?.email.toString()
+
+        // Getting the users department and making a database reference with it
+        setInformation(email)
+        val selectedDepartment = getShortDepartment()
+        val modifiedEmail = email.replace('.', '-')
+        var userSemester = "Not given"
+        val pathSemester = "/user-details/$modifiedEmail"
+        val databaseReferenceSemester = FirebaseDatabase.getInstance().getReference(pathSemester)
+        val postListenerSemester = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
 
-                val post = dataSnapshot.getValue<Routine>()
-
+                val post = dataSnapshot.getValue<UserAllData>()
 
                 if (post != null) {
-                    routineLink = post.image.toString()
-                    Glide.with(requireContext()).load(routineLink).into(binding.images)
-                }
-                else
-                {
-                    makeToast(routineLink)
+                    userSemester = post.userSemester.toString()
+                    val section = post.userSection.toString()
+                    val year = userSemester[4]
+                    val semester = userSemester[13]
+                    val path = "/routines/$selectedDepartment/year$year/semester$semester/section$section"
+                    var routineLink = "Not Found"
+                    val databaseReference = FirebaseDatabase.getInstance().getReference(path)
+                    val postListener = object : ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                            val post = dataSnapshot.getValue<Routine>()
+
+
+                            if (post != null) {
+                                routineLink = post.image.toString()
+                                if (context != null) {
+                                    Glide.with(context).load(routineLink).into(binding.images)
+                                }
+                            }
+                            else
+                            {
+                                makeToast(routineLink)
+                            }
+                        }
+
+                        override fun onCancelled(databaseError: DatabaseError) {
+
+                            makeToast("Error loading data")
+                        }
+                    }
+                    databaseReference.addValueEventListener(postListener)
                 }
             }
 
@@ -50,7 +84,9 @@ class RoutineFragment : ReplaceFragment() {
                 makeToast("Error loading data")
             }
         }
-        databaseReference.addValueEventListener(postListener)
+        databaseReferenceSemester.addValueEventListener(postListenerSemester)
+
+
         return binding.root
     }
 
